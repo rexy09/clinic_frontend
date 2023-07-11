6e0d0fd680ae9d923c6262035f09f85fea0816e6
rexy09 <fredmuju@gmail.com>
2023-07-12 00:33:51 +0300

    2 commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,7 +24,6 @@ function Layout({ children }) {
                 <NavLink className={activeNavLink} to="/">
                   Register
                 </NavLink>
-                {/* <a href="#" className="nav-link active" aria-current="page"></a> */}
               </li>
               <li>
                 <NavLink className={activeNavLink} to="/visits">
@@ -37,44 +36,7 @@ function Layout({ children }) {
                 </NavLink>
               </li>
             </ul>
-            {/* <hr /> */}
-            {/* <div className="dropdown">
-              <a
-                href="#"
-                className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <img
-                  src="https://github.com/mdo.png"
-                  alt=""
-                  width="32"
-                  height="32"
-                  className="rounded-circle me-2"
-                />
-                <strong>mdo</strong>
-              </a>
-              <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Profile
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div> */}
+            
           </div>
         </div>
         <div className="col">
@@ -88,16 +50,7 @@ function Layout({ children }) {
               </a>
 
               <ul className="nav nav-pills">
-                {/* <li className="nav-item">
-                  <a href="#" className="nav-link active" aria-current="page">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">
-                    Features
-                  </a>
-                </li> */}
+               
               </ul>
             </header>
           </div>
